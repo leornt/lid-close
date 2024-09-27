e9c98d9b27602298f1676b37968c75d106013091
Leonardo Renaut <leornt@icloud.com>
2024-09-27 16:39:24 -0300

    🎉 init

---
+++ LidClose/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LidClose
+//
+//  Created by Leonardo Renaut on 27/09/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LidClose/LidCloseApp.swift
@@ -0,0 +1,17 @@
+//
+//  LidCloseApp.swift
+//  LidClose
+//
+//  Created by Leonardo Renaut on 27/09/24.
+//
+
+import SwiftUI
+
+@main
+struct LidCloseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
